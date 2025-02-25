089af8076fddecc65134df8b3a5e58bbd8bbedc0
true-false-try <6mateo6ch@gmail.com>
2025-02-25 19:38:46 +0200

    [SECT_25] - destruction props

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,9 +1,10 @@
 function ProfileCard(props){
+    const {name, age, isMember} = props;
     return(
       <div>
-          <h3>Name: {props.name}</h3>
-          <h3>Age : {props.age}</h3>
-          <h3>Status: {props.isMember ? "Active Member" : "Guest"}</h3>
+          <h3>Name: {name}</h3>
+          <h3>Age : {age}</h3>
+          <h3>Status: {isMember ? "Active Member" : "Guest"}</h3>
       </div>
     );
 }
