a2d779e48dc70e0010fcaf1b4a67e7f23a2e7239
true-false-try <6mateo6ch@gmail.com>
2025-02-26 20:18:14 +0200

    [SECT_25] - array to props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import './App.css';
 import ProfileCard from "./components/ProfileCard.jsx";
 
 function App() {
-  return (
+    const hobby = ["Reading", "Cooking"];
+    return (
      <div>
          <h1>HELLO</h1>
-         <ProfileCard name="Alice" age={23} isMember={true} />
-         <ProfileCard name="John" age={34} isMember={false} />
+         <ProfileCard name="Alice" age={23} isMember={true} hobbies={hobby} />
+         <ProfileCard name="John" age={34} isMember={false} hobbies={hobby}/>
          <ProfileCard />
      </div>
   )
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
 
-function ProfileCard({name ="defaultUser", age=0, isMember=false}){
+function ProfileCard({name ="defaultUser", age=0, isMember=false, hobbies = ["Nothing"]}){
     return(
       <div>
           <h3>Name: {name}</h3>
           <h3>Age : {age}</h3>
           <h3>Status: {isMember ? "Active Member" : "Guest"}</h3>
+          <h3>Hobbies</h3>
+          <ul>
+              {hobbies.map((hobby, index) => {
+                 return <li key={index}>{hobby}</li>
+              })}
+          </ul>
       </div>
     );
 }
@@ -14,6 +20,7 @@ ProfileCard.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number,
     isMember: PropTypes.bool,
+    hobbies: PropTypes.array,
 }
 
 export default ProfileCard;
