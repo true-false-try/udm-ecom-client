439b59d405998c5d310d18295b6679b4ebde1279
true-false-try <6mateo6ch@gmail.com>
2025-02-26 20:35:17 +0200

    [SECT_25] - callback

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import ProfileCard from "./components/ProfileCard.jsx";
 
 function App() {
     const hobby = ["Reading", "Cooking"];
+    const  handleHobbyClick = (hobby) => {
+      alert(`You clicked on: ${hobby}`);
+    };
     return (
-     <div>
+     <div className="app-container">
          <h1>HELLO</h1>
-         <ProfileCard name="Alice" age={23} isMember={true} hobbies={hobby} />
-         <ProfileCard name="John" age={34} isMember={false} hobbies={hobby}/>
+         <ProfileCard name="Alice"
+                      age={23}
+                      isMember={true}
+                      hobbies={hobby}
+                      onHobbyClick={handleHobbyClick} />
+         <ProfileCard name="John"
+                      age={34}
+                      isMember={false}
+                      hobbies={hobby}
+                      onHobbyClick={handleHobbyClick} />
          <ProfileCard />
      </div>
   )
