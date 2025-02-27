9b7f4c8f0eb91123398fae802a7b2d852d9a37d2
true-false-try <6mateo6ch@gmail.com>
2025-02-27 23:45:56 +0200

    [SECT_26] - first webhook

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -2,13 +2,20 @@ import React, {useState} from "react";
 
 function Sandbox (){
     const [count, setCount] = useState(0);
+    const incrementValue = () => {
+        setCount(count + 1);
+    };
+    const decrementValue = () => {
+        setCount(count - 1);
+    };
+
     return(
         <div className="App">
             <h1>Hello CodeSandbox</h1>
             <h2>You clicked {count} times!</h2>
 
-            <button onClick={() => setCount(count + 1)}>Incremented</button>
-            <button onClick={() => setCount(count - 1)}>Decremented</button>
+            <button onClick={() => incrementValue()}>Incremented</button>
+            <button onClick={() => decrementValue()}>Decremented</button>
         </div>
     );
 }
