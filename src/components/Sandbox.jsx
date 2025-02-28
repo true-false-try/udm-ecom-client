44ceff40ad05c8931c865a3e079dfd9e24666a38
true-false-try <6mateo6ch@gmail.com>
2025-02-28 18:21:07 +0200

    [SECT_26] - multiply state variables

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -2,20 +2,22 @@ import React, {useState} from "react";
 
 function Sandbox (){
     const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
     const incrementValue = () => {
-        setCount(count + 1);
+        setCount(count + step);
     };
     const decrementValue = () => {
-        setCount(count - 1);
+        setCount(count - step);
     };
 
     return(
         <div className="App">
             <h1>Hello CodeSandbox</h1>
             <h2>You clicked {count} times!</h2>
-
+            <input type='number' value={step} onChange={(e) => setStep(parseInt(e.target.value))}></input>
             <button onClick={() => incrementValue()}>Incremented</button>
             <button onClick={() => decrementValue()}>Decremented</button>
+
         </div>
     );
 }
