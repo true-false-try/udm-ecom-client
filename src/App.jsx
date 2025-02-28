a519f634da417dbf55b93ca45904c655b4c60f5f
true-false-try <6mateo6ch@gmail.com>
2025-02-28 19:01:41 +0200

    [SECT_26] - work with array int state adn hok

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,34 @@ function App() {
         onHobbyClick:handleHobbyClick
     }
 
+    const [counters, setCounters] = useState([{id: 1, value: 0}]);
+    const addCounters = () => {
+        setCounters([...counters, {id: counters.length + 1, value: 0}]);
+    }
+    const incrementCounter  = (id) => {
+       setCounters(counters.map(counter =>
+            counter.id === id ? {...counter, value: counter.value + 1} : counter
+       ))
+    };
+
     return (
-     <div className="app-container">
+     /*<div className="app-container">
          <h1>HELLO</h1>
          <ProfileCard {...aliceProfile}/>
          <ProfileCard {...johnProfile}/>
          <ProfileCard />
          <Sandbox />
+     </div>*/
+     <div>
+        <button onClick={addCounters}>Add counter</button>
+         <ul>
+             {counters.map(counter => (
+                 <li key={counter.id}>
+                     Counter {counter.id}: {counter.value}
+                     <button onClick={() => incrementCounter(counter.id)}>Increment</button>
+                 </li>
+             ))}
+         </ul>
      </div>
   )
 }
