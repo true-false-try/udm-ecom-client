85f4413db22cbf70d67a274c6b6cfa68866a8944
true-false-try <6mateo6ch@gmail.com>
2025-02-28 18:34:38 +0200

    [SECT_26] - async function

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -9,6 +9,10 @@ function Sandbox (){
     const decrementValue = () => {
         setCount(count - step);
     };
+    const incrementValueTwice = () => {
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+    };
 
     return(
         <div className="App">
@@ -17,7 +21,7 @@ function Sandbox (){
             <input type='number' value={step} onChange={(e) => setStep(parseInt(e.target.value))}></input>
             <button onClick={() => incrementValue()}>Incremented</button>
             <button onClick={() => decrementValue()}>Decremented</button>
-
+            <button onClick={() => incrementValueTwice()}>+2</button>
         </div>
     );
 }
