bfb37ed4c54ab883762455e3f9752b0eb205c01c
true-false-try <6mateo6ch@gmail.com>
2025-03-03 19:32:49 +0200

    [SECT_27] - multiply event into useEffect

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -2,18 +2,25 @@ import {useEffect, useState} from "react";
 
 function UseEffect(){
     const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        document.title = `Count: ${count + 1}`;
-    }, [count]);
+    const [anotherValue, setAnotherValue] = useState(10);
     const incrementCount = () => {
         setCount(count + 1);
     };
+
+    // Right now triggering multiply event Increment Another Increment
+    // If we want only count we should use [count]
+    // If we want one use render we should use [] - empty array
+    useEffect(() => {
+        document.title = `Count: ${count + 1}`;
+        console.log("useEffect Triggered")
+        } //use on this position
+    );
 
     return(
         <div>
             <h1>useEffect Hook</h1>
             <button onClick={incrementCount}>Increment</button>
+            <button onClick={() => setAnotherValue(anotherValue + 1)}>Another Increment</button>
         </div>
     );
 }
