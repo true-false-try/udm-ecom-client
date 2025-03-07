8c5e96c1ebccdf6cfca1c58c810d747df6dba6f6
true-false-try <6mateo6ch@gmail.com>
2025-03-07 21:34:10 +0200

    [SECT_30] - updated custom hook

--- src/components/UseCounter.jsx
+++ src/components/UseCounter.jsx
@@ -1,10 +1,11 @@
 import {useState} from "react";
 
-function UseCounter(){
+function UseCounter(initialValue = 0){
     const [count, setCount] = useState(0);
     const increment = () => setCount(count+1);
     const decrement = () => setCount(count-1);
-    return{count, increment, decrement};
+    const reset = () => setCount(initialValue);
+    return{count, increment, decrement, reset};
 }
 
 export default UseCounter;
