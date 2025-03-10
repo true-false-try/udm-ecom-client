a4346b7ac06426b68b9d95857543ca7f0d77abc8
true-false-try <6mateo6ch@gmail.com>
2025-03-10 20:06:37 +0200

    [SECT_30] - loading and error

--- src/components/JsonPlaceholderAPIs.jsx
+++ src/components/JsonPlaceholderAPIs.jsx
@@ -2,11 +2,32 @@ import {useEffect, useState} from "react";
 
 function JsonPlaceholderAPIs(){
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
+        setLoading(true);
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
-            .then(json => setData(json));
+            .then(json => {
+                setData(json)
+                setLoading(false);
+                throw new Error('Something went wrong');
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+                setError('Failed to fetch the data');
+                setLoading(false);
+            })
     }, []);
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    if (error) {
+        return <p>{error}</p>
+    }
+
     return (
         <div>
             <h1>Json placeholder API</h1>
