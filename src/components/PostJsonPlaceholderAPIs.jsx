0c844ccc48e5662e612b02bf0e932845c1701f83
true-false-try <6mateo6ch@gmail.com>
2025-03-11 14:28:04 +0200

    [SECT_31] - used interceptors

--- src/components/PostJsonPlaceholderAPIs.jsx
+++ src/components/PostJsonPlaceholderAPIs.jsx
@@ -1,6 +1,16 @@
 import axios from "axios";
 import {useState} from "react";
 
+axios.interceptors.request.use(request => {
+    console.log('Starting Request')
+    return request;
+});
+
+axios.interceptors.response.use(response => {
+    console.log('Starting Response')
+    return response;
+});
+
 function PostJsonPlaceholderAPIs(){
     const [data, setData] = useState();
     const handleSubmit = (event) => {
