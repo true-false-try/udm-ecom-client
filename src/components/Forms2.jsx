05db9f7c019d31ee3c0762671bed72c8f4a6dd84
true-false-try <6mateo6ch@gmail.com>
2025-03-13 22:20:45 +0200

    [SECT_32] - used watch into hook

--- src/components/Forms2.jsx
+++ src/components/Forms2.jsx
@@ -1,8 +1,19 @@
 import {useForm} from "react-hook-form";
+import {useEffect} from "react";
 
 function Forms2(){
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit,watch} = useForm();
     const onSubmit = (data) => console.log(data);
+    //console.log(watch('name'));
+    const watchedName = watch('name');
+    const watchedEmail = watch('email');
+    useEffect(() => {
+        console.log('Name', watchedName)
+    }, [watchedName]);
+
+    useEffect(() => {
+        console.log('Email', watchedEmail)
+    }, [watchedEmail]);
     return(
         <div>
             <h1>Forms in React</h1>
