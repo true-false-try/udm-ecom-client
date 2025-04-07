3502c377fdf889becfcd7763debcac5f85db87a3
true-false-try <6mateo6ch@gmail.com>
2025-04-07 19:57:43 +0300

    [SECT_38] - created product to set layout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -40,7 +40,7 @@ const Products = () => {
               </div>
           ) : (
               <div className="min-h-[700px]">
-                <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-y-6 gap-x-6">
                     {
                         products &&
                         products.map((item,i) => <ProductCard key={i} {...item} />)
