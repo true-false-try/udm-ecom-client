d0192cae46ed38e3f8d413c1e7b9c144e085d1e2
true-false-try <6mateo6ch@gmail.com>
2025-04-11 19:18:39 +0300

    [SECT_38] - added  price and discount for card

--- src/components/ProductViewModal.jsx
+++ src/components/ProductViewModal.jsx
@@ -1,8 +1,15 @@
 import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import { useState } from 'react'
 
-function ProductViewModal() {
+function ProductViewModal({open, setOpen, product, isAvailable}) {
     let [isOpen, setIsOpen] = useState(false)
+    const {id, productName, image, description, quantity, price, discount, specialPrice} = product
+    function open() {
+        setOpen(true)
+    }
+    function close() {
+        setOpen(false)
+    }
 
     return (
         <>
@@ -10,7 +17,7 @@ function ProductViewModal() {
             <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
                 <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
                     <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-                        <DialogTitle className="font-bold">Deactivate account</DialogTitle>
+                        <DialogTitle className="font-bold">{description}</DialogTitle>
                         <Description>This will permanently deactivate your account</Description>
                         <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
                         <div className="flex gap-4">
