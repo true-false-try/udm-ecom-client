23c0c95296bb0a84150ce15514b9c7b52ef53375
true-false-try <6mateo6ch@gmail.com>
2025-04-15 21:49:31 +0300

    [SECT_38] - added Redux at app

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.jsx'
 import {StrictMode} from "react";
+import {Provider} from "react-redux";
+import store from "./store/reducers/store.js";
 
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode >
-        <App />
-    </StrictMode>
+    <Provider store={store}>
+        <StrictMode >
+            <App />
+        </StrictMode>
+    </Provider>
 )
