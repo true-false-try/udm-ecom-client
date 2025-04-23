2d1d26b710a3813f086db8ef95a40ea7a0a9d766
true-false-try <6mateo6ch@gmail.com>
2025-04-23 21:10:32 +0300

    [SECT_38] - added search bar

---
+++ src/components/Filter.jsx
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import {FiSearch} from "react-icons/fi";
+
+const Filter = () => {
+    const categories = [
+        {categoryId: 1, categoryName: "Electronics"},
+        {categoryId: 2, categoryName: "Clothing"},
+        {categoryId: 3, categoryName: "Furniture"},
+        {categoryId: 4, categoryName: "Books"},
+        {categoryId: 5, categoryName: "Toys"},
+    ];
+
+    const [category, setCategory] = useState("all");
+
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    };
+
+    return(
+        <div className="flex lg:flex-row flex-col-reverse lg:justify-between justify-center items-center gap-4">
+            {/* SEARCH BAR */}
+            <div className="relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full">
+                <input
+                    type="text"
+                    placeholder="Search Products"
+                    className="border border-grey-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:otline-none focus:ring-2 focus:ring-[#1976d2]"
+                />
+                <FiSearch  className="absolute left-3 text-slate-800 size={20}"/>
+            </div>
+
+
+        </div>
+    )
+}
+export default Filter;
