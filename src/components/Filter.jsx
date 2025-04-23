bbcd0f54f2f8f94b07bbc3b85d25f796759d50d6
true-false-try <6mateo6ch@gmail.com>
2025-04-23 21:16:00 +0300

    [SECT_38] - added form control

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {FiSearch} from "react-icons/fi";
+import {FormControl, InputLabel} from "@mui/material";
 
 const Filter = () => {
     const categories = [
@@ -23,9 +24,23 @@ const Filter = () => {
                 <input
                     type="text"
                     placeholder="Search Products"
-                    className="border border-grey-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:otline-none focus:ring-2 focus:ring-[#1976d2]"
-                />
+                    className="border border-grey-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:otline-none focus:ring-2 focus:ring-[#1976d2]"/>
                 <FiSearch  className="absolute left-3 text-slate-800 size={20}"/>
+            </div>
+
+            <div className="flex sm:flex-row flex-col gap-4 items-center">
+                <FormControl
+                    className="outlined"
+                    size="small">
+                    <InputLabel>Category</InputLabel>
+                    <Select
+                        labelId="category-select-label"
+                        value={category}
+                        onChange={handleCategoryChange}
+                        label="Category">
+                        <MenuItem value="all">All</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
 
 
