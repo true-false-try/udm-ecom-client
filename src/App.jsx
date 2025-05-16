3b7fdfdf0fecd9837371f8cf01bc2b2a5a649e98
true-false-try <6mateo6ch@gmail.com>
2025-05-16 18:33:34 +0300

    [SECT_39] - added rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 import "./index.css";
 import Products from "./components/products/Products.jsx";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/home/Home.jsx";
 
 function App() {
 
     return (
-        <Products />
+       <Router>
+           <Routes>
+                <Route path="/" element={ <Home />} />
+                <Route path="/products" element={ <Products />} />
+           </Routes>
+       </Router>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,11 @@ import App from './App.jsx'
 import {StrictMode} from "react";
 import {Provider} from "react-redux";
 import store from "./store/reducers/store.js";
-import {BrowserRouter} from "react-router-dom";
-
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <StrictMode >
-            <BrowserRouter >
-                <App />
-            </BrowserRouter>
+            <App />
         </StrictMode>
     </Provider>
 )
