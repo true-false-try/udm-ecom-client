108ad5230c7988ff6b00118e245e8b326db48146
true-false-try <6mateo6ch@gmail.com>
2025-05-25 21:59:27 +0300

    [SECT_39] - Navbar component has been supplement

--- src/components/shared/Nvbar.jsx
+++ src/components/shared/Nvbar.jsx
@@ -1,9 +1,13 @@
 import {Link, useLocation} from "react-router-dom";
-import {FaShoppingCart, FaStore} from "react-icons/fa";
+import {FaShoppingCart, FaSignInAlt, FaStore} from "react-icons/fa";
 import {Badge} from "@mui/material";
+import {useState} from "react";
+import {RxCross2} from "react-icons/rx";
+import {IoIosMenu} from "react-icons/io";
 
 const Navbar = () => {
     const path =  useLocation().pathname
+    const [navbarOpen, setNavbarOpen] = useState(false);
     return (
         <div className="h-[70px] bg-custom-gradient text-white z-50 flex items-center top-0">
             <div className="lg:px-14 sm:px-8 px-4 w-full flex items-center justify-between">
@@ -12,7 +16,9 @@ const Navbar = () => {
                     <span className="font-[Poppins]">E-Shop</span>
                 </Link>
 
-                <ul className="flex text-slate-800 absolute left-0 gap-4">
+                <ul className={`flex sm:gap-10 gap-4 sm:items-center text-slate-800 sm:static absolute left-0-top-[70px] sm:shadow-none shadow-md ${
+                    navbarOpen ? 'sm' : 'md'
+                }` }>
                     <li className="font-[500] transition-all duration-150">
                         <Link className={`${
                             path === "/" ? "text-white font-semibold" : "text-grey-200"
@@ -64,7 +70,25 @@ const Navbar = () => {
                             </Badge>
                         </Link>
                     </li>
+
+                    <li className="font-[500] transition-all duration-150">
+                        <Link className="flex items-center space-x-2 px-4 py-[6px] bg-greadient-to-r ease-in-out transform"
+                              to={"/login"}>
+                           <FaSignInAlt />
+                            <span>Login</span>
+                        </Link>
+                    </li>
                 </ul>
+
+                <button onClick={() => setNavbarOpen(!navbarOpen)}
+                className="sm:hidden flex items-center sm:mt-0 mt-2">
+                    {navbarOpen ? (
+                        <RxCross2 className="text-white text-3xl"/>
+                    ) : (
+                        <IoIosMenu className="text-white text-3xl"/>
+                        )}
+
+                </button>
             </div>
         </div>
     )
