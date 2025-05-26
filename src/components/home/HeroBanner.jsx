35dc95d832e2cc865f908c75c8e34687e71083f6
true-false-try <6mateo6ch@gmail.com>
2025-05-26 18:31:13 +0300

    [SECT_39] - fixed problem with painting page

--- src/components/home/HeroBanner.jsx
+++ src/components/home/HeroBanner.jsx
@@ -6,14 +6,13 @@ import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import 'swiper/css/effect-fade'
 import 'swiper/css/autoplay'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { motion } from "framer-motion";
+
 
 const colors = ["bg-banner-color-1, bg-banner-color-2", "bg-banner-color-3"];
 
 export default () => {
-    import { Swiper, SwiperSlide } from 'swiper/vue';
-
-    // Import Swiper styles
-    import 'swiper/css';
 
     const HeroBanner = () => {
         return (
@@ -22,8 +21,8 @@ export default () => {
                         autoplay={{delay:4000, disableOnInteraction:false}}
                         navigation
                         modules={[Pagination, EffectFade, Navigation, Autoplay]}
-                        pagination={{clickable:true}}
-                        scrollbar=>{{draggable:true}}
+                        pagination={{ clickable:true }}
+                        scrollbar={{ draggable: true }}
                         slidesPerView={1}>
                     {bannerList.map((item, index) => (
                         <SwiperSlide key={item.id}>
@@ -33,7 +32,7 @@ export default () => {
                                         <div className="text-center">
                                             <h3 className='text-3xl text-white font-bold'>{item.title}</h3>
                                             <h1 className='text-5xl text-white-bold mt-2'></h1>
-                                            <p className='text-white font-bold mt-4'>${item.description}</p>
+                                            <p className='text-white font-bold mt-4'>{item.description}</p>
                                             <Link className='mt-6 inline-block bg-black text-white py-2 px-4 rounded hover:bg-g'
                                                   to="/products">
                                                 Shop
