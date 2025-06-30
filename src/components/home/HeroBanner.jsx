a61501cde52194d60f4be937c978b30f782fd56c
true-false-try <6mateo6ch@gmail.com>
2025-06-30 20:02:48 +0300

    [SECT_42] - reset Link

--- src/components/home/HeroBanner.jsx
+++ src/components/home/HeroBanner.jsx
@@ -29,7 +29,15 @@ export default () => {
                             <div className={`carousel-item rounded-md sm:h-[500px] h-96 ${colors[index]}`}>
                                 <div className="flex flex-col items-center justify-center">
                                     <div className=" hidden lg:flex flex-col items-center w-1/2 p-8">
-                                        checker
+                                        <div className="text-center">
+                                            <h3 className='text-3xl text-white font-bold'>{item.title}</h3>
+                                            <h1 className='text-5xl text-white-bold mt-2'></h1>
+                                            <p className='text-white font-bold mt-4'>{item.description}</p>
+                                            <Link className='mt-6 inline-block bg-black text-white py-2 px-4 rounded hover:bg-g'
+                                                  to="/products">
+                                                Shop
+                                            </Link>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="w-full flex justify-center lg:w-1/2 p-4">
