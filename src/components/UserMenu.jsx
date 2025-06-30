18bcebe1f290c67e64a2f14490a179d63877a52f
true-false-try <6mateo6ch@gmail.com>
2025-06-30 20:45:21 +0300

    [SECT_42] - displayed user's menu if authenticated

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React, {useState} from 'react';
+import { Button, Menu, MenuItem, Avatar, Box, Typography } from '@mui/material';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import {Link} from "react-router-dom";
+import {BiUser} from "react-icons/bi";
+import {useSelector} from "react-redux";
+import {FaShoppingCart} from "react-icons/fa";
+import {IoExitOutline} from "react-icons/io5";
 
 const UserMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const { user } = useSelector((state) => state.auth);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const logOutHandler = () => {
+
+    };
+
     return (
-        <PopupState variant="popover" popupId="demo-popup-menu">
-            {(popupState) => (
-                <React.Fragment>
-                    <Button variant="contained" {...bindTrigger(popupState)}>
-                        Dashboard
-                    </Button>
-                    <Menu {...bindMenu(popupState)}>
-                        <MenuItem onClick={popupState.close}>Profile</MenuItem>
-                        <MenuItem onClick={popupState.close}>My account</MenuItem>
-                        <MenuItem onClick={popupState.close}>Logout</MenuItem>
-                    </Menu>
-                </React.Fragment>
-            )}
-        </PopupState>
-    )
-}
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Button variant="contained">Dashboard</Button>
+
+            <PopupState variant="popover" popupId="user-menu" parentPopupState={} disableAutoFocus={}>
+                {(popupState) => (
+                    <React.Fragment>
+                        <Avatar
+                            alt="User Avatar"
+                            src=""
+                            {...bindTrigger(popupState)}
+                            sx={{ cursor: 'pointer' }}
+                        />
+                        <Menu {...bindMenu(popupState)}>
+                            <Link to="/profile">
+                                <MenuItem className="flex gap-2" onClick={popupState.close}>
+                                    <BiUser className='text-xl' />
+                                    <span className='fond-bold text-[16px] nt-1'>
+                                        {user?.username}
+                                    </span>
+                                </MenuItem>
+                            </Link>
+                            <Link to="/profile/orders">
+                                <MenuItem className="flex gap-2" onClick={handleClose}>
+                                    <FaShoppingCart className='text-xl' />
+                                    <span className='font-semibold'>
+                                        Order
+                                    </span>
+                                </MenuItem>
+                            </Link>
+                            <MenuItem className="flex gap-2" onClick={logOutHandler}>
+                                <div className="font-semibold w-full flex gap-2 items-center bg-button-gradient px-4 py-1 text-white rounded-sm">
+                                    <IoExitOutline className='text-xl' />
+                                    <span className='fond-bold text-[16px] nt-1'>
+                                       LogOut
+                                    </span>
+                                </div>
+                            </MenuItem>
+                        </Menu>
+                    </React.Fragment>
+                )}
+            </PopupState>
+        </Box>
+    );
+};
 
 export default UserMenu;
