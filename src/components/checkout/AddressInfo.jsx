99ef9bfd2d642013625bf1373fb3464939ae3c54
true-false-try <6mateo6ch@gmail.com>
2025-07-26 22:36:33 +0300

    [SECT_44] - restore h1

--- src/components/checkout/AddressInfo.jsx
+++ src/components/checkout/AddressInfo.jsx
@@ -9,6 +9,7 @@ const AddressInfo = () => {
         <div className='pt-4'>
             {noAddressExist ? (
                 <div className='p-6 rounded-lg max-w-md mx-auto flex-col items-center justify-center'>
+                    <FaAddressBook size={50} className='text-gray-500 nb-4'/>
                     <h1 className='mb-2 text-slate-800 text-center font-semibold text-2xl'>
                         No addresses added yet
                     </h1>
