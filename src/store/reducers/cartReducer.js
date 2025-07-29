6ecb050fbd0f69000d94636b07356d1a96a49639
true-false-try <6mateo6ch@gmail.com>
2025-07-29 22:36:28 +0300

    [SECT_44] - refactored component

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -2,43 +2,43 @@ const initialState = {
     cart: [],
     totalPages: 0,
     cartId: null,
-}
+};
 
 export const cartReducer = (state = initialState, action) => {
     switch (action.type) {
-        case "ADD_CART":
-            const product = action.payload;
+        case "ADD_CART": {
+            const productToAdd = action.payload;
             const existingProduct = state.cart.find(
-                (item) => item.id === product.id
+                (item) => item.id === productToAdd.id
             );
 
             if (existingProduct) {
-                const updatedCart = state.cart.map((item) => {
-                    if (item.id === productToAdd.id) {
-                        return productToAdd;
-                    } else {
-                        return item;
-                    }
-                });
+                const updatedCart = state.cart.map((item) =>
+                    item.id === productToAdd.id ? productToAdd : item
+                );
                 return {
                     ...state,
-                    carts: updatedCart,
-                }
+                    cart: updatedCart,
+                };
             } else {
-                const newCart = [...state.cart, id];
+                const newCart = [...state.cart, productToAdd];
                 return {
                     ...state,
-                    carts: newCart,
-                }
+                    cart: newCart,
+                };
             }
-        case "REMOVE_CART":
+        }
+
+        case "REMOVE_CART": {
             return {
                 ...state,
                 cart: state.cart.filter(
-                    (item) =>  item.id !== action.payload.id
+                    (item) => item.id !== action.payload.id
                 ),
-            }
+            };
+        }
+
         default:
             return state;
     }
-}
+};
