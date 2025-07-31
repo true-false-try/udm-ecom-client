22a3d6b301ccedecf773fc7a71e6670c5a238fdc
true-false-try <6mateo6ch@gmail.com>
2025-07-31 21:28:15 +0300

    [SECT_44] - refactor

--- src/components/checkout/AddressInfo.jsx
+++ src/components/checkout/AddressInfo.jsx
@@ -1,30 +1,36 @@
-import {Skeleton} from "@mui/material";
-import {FaAddressBook} from "react-icons/fa";
+import { Skeleton } from "@mui/material";
+import { FaAddressBook } from "react-icons/fa";
 
 const AddressInfo = () => {
     const noAddressExist = true;
     const isLoading = false;
 
     return (
-        <div className='pt-4'>
-            {noAddressExist ? (
-                <div className='p-6 rounded-lg max-w-md mx-auto flex-col items-center justify-center'>
-                    <h1 className='mb-2 text-slate-800 text-center font-semibold text-2xl'>
+        <div className="pt-4">
+            {isLoading ? (
+                <div className="max-w-md mx-auto p-6">
+                    <Skeleton variant="text" height={40} />
+                    <Skeleton variant="text" height={30} />
+                </div>
+            ) : noAddressExist ? (
+                <div className="p-6 rounded-lg max-w-md mx-auto flex flex-col items-center justify-center text-center bg-gray-100 shadow">
+                    <FaAddressBook className="text-4xl text-blue-500 mb-4" />
+                    <h1 className="mb-2 text-slate-800 font-semibold text-2xl">
                         No addresses added yet
                     </h1>
-                    <p className='mb-2 text-slate-800 text-center font-semibold text-2xl'>
-                        Please added your address to complete purchase
+                    <p className="text-slate-700 text-base">
+                        Please add your address to complete the purchase
                     </p>
                 </div>
             ) : (
-                <div className='relative p-6 rounded-lg max-w-md mx-auto'>
-                    <h1 className='text-slate-800 taxt-center font-bold text-2xl'>
-                        Select address
+                <div className="relative p-6 rounded-lg max-w-md mx-auto bg-white shadow">
+                    <h1 className="text-slate-800 text-center font-bold text-2xl">
+                        Select an address
                     </h1>
                 </div>
             )}
         </div>
     );
-}
+};
 
 export default AddressInfo;
