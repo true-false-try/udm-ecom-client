b18e24f9d45255ef919af2687b889aeca5bfee6f
true-false-try <6mateo6ch@gmail.com>
2025-09-04 17:01:22 +0300

    [SECT_44] - App restore

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,10 @@ function App() {
                     <Route path="/contact" element={ <Contact />} />
                     <Route path="/cart" element={ <Cart />} />
                     <Route path="/checkout" element={ <Checkout />} />
-                    
+                    <Route path='/' element={<PrivateRoute publicPage />}>
+                        <Route path="/login" element={ <LogIn />} />
+                        <Route path="/register" element={ <Register />} />
+                    </Route>
                 </Routes>
             </Router>
             <Toaster position='bottom-center' />
