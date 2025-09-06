e57ed2d519b490294e65c98276a4026b68e61ebd
true-false-try <6mateo6ch@gmail.com>
2025-09-06 20:52:59 +0300

    [SECT_44] - App restore

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -26,6 +26,48 @@ const Cart = () => {
                 <p className="text-lg text-gray-600 mt-2">All your selected items</p>
             </div>
 
+            <div className="grid md:grid-cols-5 grid-cols-4 gap-4 pb-2 font-semibold items-center">
+                <div className="md:col-span-2 justify-self-start text-lg text-slate-800 lg-ps-4">
+                    Product
+                </div>
+                <div className="md:col-span-2 justify-self-start text-lg text-slate-800 lg-ps-4">
+                    Quantity
+                </div>
+                <div className="md:col-span-2 justify-self-start text-lg text-slate-800 lg-ps-4">
+                    Price
+                </div>
+                <div className="md:col-span-2 justify-self-start text-lg text-slate-800 lg-ps-4">
+                    Total
+                </div>
+
+                <div>
+                    {cart && cart.length > 0 &&
+                        cart.map((item, i) => <ItemContent key={i} {...item}/>)}
+                </div>
+
+                <div className="border-t-[1.5px] border-slate-200 py-4 sm:flex sm:flex-row sm:px-0 px-2 flex-col sm:justify-between">
+                    <div></div>
+                    <div className="flex text-sm gap-l flex-col">
+                        <div className="flex justify-between w-full md:text-lg text-sm font-semibold">
+                            <span>Subtotal</span>
+                            <span>{formatPrice(newCart?.totalPrice)}</span>
+                        </div>
+                    </div>
+                    <p className="text-slate-500">
+                        Taxes and shipping calculated at checkout
+                    </p>
+                    <Link className="w-full flex justify-end" to="/checkout">
+                        <button
+                            onClick={() => {}}
+                            className="font-semibold w-[300px] py-2 px-4 rounded-sm text-white flex items-center justify-center gap-2 hover:text-gray-300 transition duration-500">
+                            <MdShoppingCart size={20} />
+                            Checkout
+                        </button>
+                    </Link>
+
+                </div>
+            </div>
+
 
         </div>
     )
