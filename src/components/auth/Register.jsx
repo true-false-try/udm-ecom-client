53b6f6f536ef5de87699e7948122a48e783bcf0b
true-false-try <6mateo6ch@gmail.com>
2025-09-18 22:50:56 +0300

    [SECT_44] - added keywords

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -40,7 +40,18 @@ const Register = () => {
                             errors={errors}
                         />
                     </div>
-                    
+                    <div>
+                        <InputField
+                            label="Password"
+                            required
+                            id="password"
+                            type="password"
+                            message="*Password is required"
+                            placeholder="Enter your password"
+                            register={register}
+                            errors={errors}
+                        />
+                    </div>
                     <div>
                         <InputField
                             label="Email"
