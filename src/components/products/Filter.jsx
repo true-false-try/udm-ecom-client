61535ba7217729889ae0f9cda68a73a43d4bd527
true-false-try <6mateo6ch@gmail.com>
2025-09-23 20:55:35 +0300

    [SECT_44] - useEffect testing

--- src/components/products/Filter.jsx
+++ src/components/products/Filter.jsx
@@ -14,7 +14,19 @@ const Filter = ({categories}) => {
     const navigate = useNavigate();
     const params = new URLSearchParams(searchParams);
 
-
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            if(searchParams){
+               searchParams.set("keyword", searchTerm);
+            } else {
+                searchParams.delete("keyword")
+            }
+            navigate(`${pathname}?${searchParams.toString()}`);
+        }, 700);
+        return () => {
+            clearTimeout(handler);
+        }
+    }, [searchParams, searchTerm, navigate, pathname])
 
     const handleCategoryChange = (event) => {
         const selectedCategory = event.target.value;
