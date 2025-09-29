723d77202f9c09f8bee2e46dc0147f9d1d720045
true-false-try <6mateo6ch@gmail.com>
2025-09-29 22:18:09 +0300

    [SECT_44] - useEffect testing

--- src/components/checkout/AddressInfo.jsx
+++ src/components/checkout/AddressInfo.jsx
@@ -26,7 +26,7 @@ const AddressInfo = () => {
                 <div className="relative p-6 rounded-lg max-w-md mx-auto bg-white shadow">
                     <h1 className="text-slate-800 text-center font-bold text-2xl">
                         Select an address
-                    </h1>1
+                    </h1>
                 </div>
             )}
         </div>
