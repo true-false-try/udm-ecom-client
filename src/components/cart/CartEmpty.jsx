e77d7efd4c8f4c02da6960276b1689bddb25a12a
true-false-try <6mateo6ch@gmail.com>
2025-09-30 13:48:11 +0300

    [SECT_44] - empty cart

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -4,15 +4,7 @@ import {Link} from "react-router-dom";
 const CartEmpty = () => {
     return(
         <div className="min-h-[800px] flex flex-col items-center justify-center">
-            <div className="flex flex-col items-center items-center">
-                <MdShoppingCart size={80} className="mb-4 text-slate-500" />
-                <div className="text-3xl font-bold text-slate-700">
-                    Your cart is empty
-                </div>
-                <div className="text-lg text-slate-500 md-2">
-                    Add some products to get started
-                </div>
-            </div>
+
             <div className="mt6">
                 <Link to={"/"}
                       className="flex gap-2 items-center text-blue-500 hover:text-blue-600 transition">
