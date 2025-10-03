6dc78d3fe25179f1a05a75cc38c1cf2d167c7822
true-false-try <6mateo6ch@gmail.com>
2025-10-03 17:09:03 +0300

    [SECT_44] - rest

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -6,7 +6,31 @@ const initialState = {
 
 export const cartReducer = (state = initialState, action) => {
     switch (action.type) {
+        case "ADD_CART":
+            const product = action.payload;
+            const existingProduct = state.cart.find(
+                (item) => item.id === product.id
+            );
 
+            if (existingProduct) {
+                const updatedCart = state.cart.map((item) => {
+                    if (item.id === productToAdd.id) {
+                        return productToAdd;
+                    } else {
+                        return item;
+                    }
+                });
+                return {
+                    ...state,
+                    carts: updatedCart,
+                }
+            } else {
+                const newCart = [...state.cart, id];
+                return {
+                    ...state,
+                    carts: newCart,
+                }
+            }
         case "REMOVE_CART":
             return {
                 ...state,
