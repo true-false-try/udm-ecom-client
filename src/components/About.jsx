85816cb67578f3a90de63cdcbfb00fd6b3cf1e4a
true-false-try <6mateo6ch@gmail.com>
2025-10-26 21:54:16 +0200

    [SECT_44] - removed div from About and added return into UserMenu.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -34,7 +34,34 @@ const About = () => {
             <h1 className="text-slate-800 text-4xl font-bold text-center mb-12">
                 About Us
             </h1>
+            <div className="flex flex-col lg:flex-row justify-between items-center mb-12">
+                <div className="w-full lg:w-1/2 text-center md:text-left">
+                    <p className="text-lg mb-4">Welcome to our e-commerce store!</p>
+                </div>
+                <div className="w-full lg:w-1/2 mb-6 md:mb-0">
+                    <img src="https://embarkx.com/sample/placeholder.png"
+                         alt="About Us" className="w-full h-auto rounted-lg shadow-lg transform transitional duration-300 hover:scale-105" />
+                </div>
 
+                <div className="py-7 space-y-8">
+                    <h1 className="text-slate-800 text-4xl font-bold text-center">
+                        Our Products
+                    </h1>
+                    <div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-y-6 gap-x-6'>
+                        {
+                            products.map((product) => (
+                                <ProductCard key={index}
+                                             image={product.image}
+                                             productName={product.name}
+                                             description={product.description}
+                                             specialPrice={product.specialPrice}
+                                             price={product.price}
+                                             about={product.about}/>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
