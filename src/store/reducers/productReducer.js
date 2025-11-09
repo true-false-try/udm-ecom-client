e0e91a943c9cae1e626b15879e80c4caf2db3179
true-false-try <6mateo6ch@gmail.com>
2025-11-09 22:28:01 +0200

    [SECT_44] - added product pattern

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -22,7 +22,19 @@ export const productReducer = (state = initialState, action) => {
                 }
             };
 
-
+        case "FETCH_CATEGORIES":
+            return {
+                ...state,
+                categories: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalElements: action.totalElements,
+                    totalPages: action.totalPages,
+                    lastPage: action.lastPage,
+                }
+            };
         default:
             return state;
     }
