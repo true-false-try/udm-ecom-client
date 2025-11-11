927a326ad48db6682aef0e6dd7bdde7e52224f39
true-false-try <6mateo6ch@gmail.com>
2025-11-11 12:43:31 +0200

    [SECT_44] - not button and dive usege

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -49,18 +49,7 @@ const LogIn = () =>  {
                         />
                     </div>
 
-                    <button disabled={loader}
-                            className="bg-button-gradient flex gap-2 items-center justify-center font-semibold text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
-                            type="submit">
-                        {loader ? (
-                            <>
-                                <Spinners /> Loading...
-                            </>
-                        ) : (
-                            <>Login</>
-                        )}
 
-                    </button>
                 </div>
 
                 <p className="text-center text-sm text-slate-700 mt-6">
