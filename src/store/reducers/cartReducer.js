f1f29ce37d61cbcd0b5475cbaca6e84791008778
true-false-try <6mateo6ch@gmail.com>
2025-11-15 19:07:24 +0200

    [SECT_44] - switched success

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -31,6 +31,13 @@ export const cartReducer = (state = initialState, action) => {
                     carts: newCart,
                 }
             }
+        case "REMOVE_CART":
+            return {
+                ...state,
+                cart: state.cart.filter(
+                    (item) =>  item.id !== action.payload.id
+                ),
+            }
         default:
             return state;
     }
