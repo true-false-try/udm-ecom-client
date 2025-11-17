7704792ee11f6c4b5f1b571aa88c86810f24ddef
true-false-try <6mateo6ch@gmail.com>
2025-11-17 23:49:25 +0200

    [SECT_44] - not button and dive usege

--- src/store/reducers/errorReducer.js
+++ src/store/reducers/errorReducer.js
@@ -7,8 +7,18 @@ const initialState = {
 
 export const errorReducer = (state = initialState, action) => {
     switch (action.type) {
-
-
+        case "IS_FETCHING":
+            return {
+                ...state,
+                isLoading: true,
+                errorMessage: null
+            }
+        case "IS_SUCCESS":
+            return {
+                ...state,
+                isLoading: true,
+                errorMessage: null,
+            }
         case "IS_ERROR":
             return {
                 ...state,
