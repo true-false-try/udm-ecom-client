834fb51f8a852be63435c557b5ecfac9ff8f2889
true-false-try <6mateo6ch@gmail.com>
2026-01-01 21:15:34 +0200

    [SECT_44] - broken info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,6 +38,10 @@ const About = () => {
                 <div className="w-full lg:w-1/2 text-center md:text-left">
                     <p className="text-lg mb-4">Welcome to our e-commerce store!</p>
                 </div>
+                <div className="w-full lg:w-1/2 mb-6 md:mb-0">
+                    <img src="https://embarkx.com/sample/placeholder.png"
+                         alt="About Us" className="w-full h-auto rounted-lg shadow-lg transform transitional duration-300 hover:scale-105" />
+                </div>
 
                 <div className="py-7 space-y-8">
                     <h1 className="text-slate-800 text-4xl font-bold text-center">
