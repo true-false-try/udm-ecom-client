278c26774c15cf246261943c30311d71537c0408
true-false-try <6mateo6ch@gmail.com>
2026-01-24 22:15:34 +0200

    [SECT-44]  - remove contact us

--- src/components/products/Filter.jsx
+++ src/components/products/Filter.jsx
@@ -34,6 +34,15 @@ const Filter = ({categories}) => {
     return(
         <div className="flex lg:flex-row flex-col-reverse lg:justify-between justify-center items-center gap-4">
             {/* SEARCH BAR */}
+            <div className="relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full">
+                <input
+                    type="text"
+                    placeholder="Search Products"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="border border-grey-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:otline-none focus:ring-2 focus:ring-[#1976d2]"/>
+                <FiSearch  className="absolute left-3 text-slate-800 size={20}"/>
+            </div>
 
             <div className="flex sm:flex-row flex-col gap-4 items-center">
                 <FormControl
