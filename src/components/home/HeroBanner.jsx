c825b46abcced28c142648317aeed5d001a77743
true-false-try <6mateo6ch@gmail.com>
2026-03-03 19:34:14 +0200

    [SECT_44] - swiper fix

--- src/components/home/HeroBanner.jsx
+++ src/components/home/HeroBanner.jsx
@@ -1,13 +1,11 @@
 import {bannerList} from "../../utils/index.js";
-import {Autoplay, EffectFade, Pagination} from "swiper/modules";
 import {Link} from "react-router-dom";
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import 'swiper/css/effect-fade'
 import 'swiper/css/autoplay'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { motion } from "framer-motion";
+import {SwiperSlide } from 'swiper/react';
 
 
 const colors = ["bg-banner-color-1, bg-banner-color-2", "bg-banner-color-3"];
@@ -17,13 +15,6 @@ export default () => {
     const HeroBanner = () => {
         return (
             <div className="py-2 round-md">
-                <Swiper grabCursor={true}
-                        autoplay={{delay:4000, disableOnInteraction:false}}
-                        navigation
-                        modules={[Pagination, EffectFade, Navigation, Autoplay]}
-                        pagination={{ clickable:true }}
-                        scrollbar={{ draggable: true }}
-                        slidesPerView={1}>
                     {bannerList.map((item, index) => (
                         <SwiperSlide key={item.id}>
                             <div className={`carousel-item rounded-md sm:h-[500px] h-96 ${colors[index]}`}>
@@ -46,7 +37,6 @@ export default () => {
                             </div>
                         </SwiperSlide>
                     ))}
-                </Swiper>
             </div>
         )
     }
