b900ca57ea7a6a932c275d4e7cb50227a79d7694
true-false-try <6mateo6ch@gmail.com>
2026-03-04 22:05:01 +0200

    [SECT_44] - herro banner recomming

--- src/components/home/HeroBanner.jsx
+++ src/components/home/HeroBanner.jsx
@@ -1,11 +1,13 @@
 import {bannerList} from "../../utils/index.js";
+import {Autoplay, EffectFade, Pagination} from "swiper/modules";
 import {Link} from "react-router-dom";
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import 'swiper/css/effect-fade'
 import 'swiper/css/autoplay'
-import {SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { motion } from "framer-motion";
 
 
 const colors = ["bg-banner-color-1, bg-banner-color-2", "bg-banner-color-3"];
@@ -15,6 +17,13 @@ export default () => {
     const HeroBanner = () => {
         return (
             <div className="py-2 round-md">
+                <Swiper grabCursor={true}
+                        autoplay={{delay:4000, disableOnInteraction:false}}
+                        navigation
+                        modules={[Pagination, EffectFade, Navigation, Autoplay]}
+                        pagination={{ clickable:true }}
+                        scrollbar={{ draggable: true }}
+                        slidesPerView={1}>
                     {bannerList.map((item, index) => (
                         <SwiperSlide key={item.id}>
                             <div className={`carousel-item rounded-md sm:h-[500px] h-96 ${colors[index]}`}>
@@ -37,6 +46,7 @@ export default () => {
                             </div>
                         </SwiperSlide>
                     ))}
+                </Swiper>
             </div>
         )
     }
