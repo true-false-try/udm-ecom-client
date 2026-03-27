5f9eabe39c6a22e5be888fb13940faf3ad68745d
true-false-try <6mateo6ch@gmail.com>
2026-03-27 22:32:38 +0200

    [SECT_44] - login register need upgrade

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -28,17 +28,6 @@ const LogIn = () =>  {
                     </h1>
                     <hr className="mt-2 mb-5 text-black" />
                 </div>
-
-                <p className="text-center text-sm text-slate-700 mt-6">
-                    Don't have an account?
-                    <Link className="font-semibold underline hover:text-black"
-                          to="/register"
-                    >
-                        <span>
-                            SignUp
-                        </span>
-                    </Link>
-                </p>
             </form>
         </div>
     );
