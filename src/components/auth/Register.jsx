246ee45d34141efb105c069c198489fe309bffea
true-false-try <6mateo6ch@gmail.com>
2026-04-04 20:20:11 +0300

    [SECT_44] - import dleted

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,7 +1,6 @@
 import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
 import {useForm} from "react-hook-form";
-import {AiOutlineLogin} from "react-icons/ai";
 import {useDispatch} from "react-redux";
 
 const LogIn = () =>  {
@@ -9,9 +8,7 @@ const LogIn = () =>  {
     const dispatch = useDispatch();
     const [loader, setLoader] = useState(false);
     const {
-        register,
         handleSubmit,
-        reset,
         formState: {errors},
     } = useForm({
         mode: "onTouched",
