f92d498abae746729b6b8e99f27fa9430dd56ef1
true-false-try <6mateo6ch@gmail.com>
2026-04-06 21:12:59 +0300

    [SECT_44] - restart rm fields

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,6 +1,7 @@
 import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
 import {useForm} from "react-hook-form";
+import {AiOutlineLogin} from "react-icons/ai";
 import {useDispatch} from "react-redux";
 
 const LogIn = () =>  {
@@ -8,7 +9,9 @@ const LogIn = () =>  {
     const dispatch = useDispatch();
     const [loader, setLoader] = useState(false);
     const {
+        register,
         handleSubmit,
+        reset,
         formState: {errors},
     } = useForm({
         mode: "onTouched",
@@ -18,6 +21,13 @@ const LogIn = () =>  {
         <div className="min-h-[calc(100vh-64px)] flex justify-center items-center">
             <form onSubmit={handleSubmit(loginHandle)}
                   className="sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md">
+                <div className="flex flex-col items-center justify-center space-y-4">
+                    <AiOutlineLogin  className="text-slate-800 text-5xl" />
+                    <h1 className="text-slate-800 text-center font-montserrat lg:text-3xl text-2xl font-bold">
+                        Login Here
+                    </h1>
+                    <hr className="mt-2 mb-5 text-black" />
+                </div>
 
                 <p className="text-center text-sm text-slate-700 mt-6">
                     Don't have an account?
