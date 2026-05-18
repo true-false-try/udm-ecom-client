1eebef11dfcc256c6063247a103a08fde9eb2541
true-false-try <6mateo6ch@gmail.com>
2026-05-18 17:15:19 +0300

    [SECT_44] - empty shield

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -5,7 +5,6 @@ const CartEmpty = () => {
     return(
         <div className="min-h-[800px] flex flex-col items-center justify-center">
             <div className="flex flex-col items-center items-center">
-                <MdShoppingCart size={80} className="mb-4 text-slate-500" />
                 <div className="text-3xl font-bold text-slate-700">
                     Your cart is empty
                 </div>
