3edb763e04c75ba1383fb1a61e9ff026ff9a7dbe
true-false-try <6mateo6ch@gmail.com>
2026-06-01 14:26:18 +0300

    [SECT_44] - register div center

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -21,13 +21,6 @@ const LogIn = () =>  {
         <div className="min-h-[calc(100vh-64px)] flex justify-center items-center">
             <form onSubmit={handleSubmit(loginHandle)}
                   className="sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md">
-                <div className="flex flex-col items-center justify-center space-y-4">
-                    <AiOutlineLogin  className="text-slate-800 text-5xl" />
-                    <h1 className="text-slate-800 text-center font-montserrat lg:text-3xl text-2xl font-bold">
-                        Login Here
-                    </h1>
-                    <hr className="mt-2 mb-5 text-black" />
-                </div>
 
                 <p className="text-center text-sm text-slate-700 mt-6">
                     Don't have an account?
